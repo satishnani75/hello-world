5abe0371e50641c7fd9afd2cdb071e21443949be
satish-lead <satishnani75@gmail.com>
2022-01-30 18:25:11 +0530

    initial commit

---
+++ src/main/java/com/app/exp/MyappRestController.java
@@ -0,0 +1,26 @@
+package com.app.exp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class MyappRestController {
+
+	
+	@GetMapping("/show")
+	public String show() {
+		
+		return "this is swagger show";
+		
+	}
+	
+	
+	@GetMapping("/get")
+	public String get() {
+		
+		
+		return " this swagger get";
+	}
+}
